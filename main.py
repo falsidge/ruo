1bb4b597dae33b441263bf76db4628796627bdf5
falsidge <tesetings@gmail.com>
2023-03-17 22:26:39 -0700

    add fcm token register endpoint

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+import pathlib
 from Crypto.PublicKey import RSA
 from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA512
@@ -14,11 +16,11 @@ import requests
 
 
 class Client:
-    def __init__(self,akey=None, pkey=None,host=None,code=None, response=None,keyfile=None):
+    def __init__(self, akey=None, pkey=None, host=None, code=None, response=None, keyfile=None):
         if keyfile:
             self.import_key(keyfile)
         else:
-            self.pubkey = RSA.generate(2048) 
+            self.pubkey = RSA.generate(2048)
 
         self.pkey = pkey
         self.akey = akey
@@ -29,13 +31,12 @@ class Client:
             self.read_code(code)
         if response:
             self.import_response(response)
-    
+
     def __str__(self):
         return repr(self)
-    
-    def __repr__(self) -> str:
-        return "Client("+",".join([(self.__dict__[i] or '') and (i+'='+self.__dict__[i]) for i in ["akey","pkey","host"]])+")"
 
+    def __repr__(self) -> str:
+        return "Client("+",".join([(self.__dict__[i] or '') and (i+'='+self.__dict__[i]) for i in ["akey", "pkey", "host"]])+")"
 
     def import_key(self, keyfile):
         if issubclass(type(keyfile), io.IOBase):
@@ -46,24 +47,25 @@ class Client:
             except ValueError:
                 with open(keyfile, "rb") as f:
                     self.pubkey = RSA.import_key(f.read())
+
     def export_key(self, file):
         if type(file) is str:
-            with open(file,"wb") as f:
+            with open(file, "wb") as f:
                 f.write(self.pubkey.export_key("PEM"))
         else:
             file.write(self.pubkey.export_key("PEM"))
-    
+
     def read_code(self, code):
-        code,host = map(lambda x:x.strip("<>"), code.split("-"))
+        code, host = map(lambda x: x.strip("<>"), code.split("-"))
         missing_padding = len(host) % 4
         if missing_padding:
-            host += '='* (4 - missing_padding)
+            host += '=' * (4 - missing_padding)
         self.code = code
         self.host = base64.decodebytes(host.encode("ascii")).decode('ascii')
-    
+
     def import_response(self, response):
         if type(response) is str:
-            with open(response,"r") as f:
+            with open(response, "r") as f:
                 response = json.load(f)
         if "response" in response:
             response = response["response"]
@@ -74,7 +76,7 @@ class Client:
             self.host = self.info["host"]
         self.akey = response["akey"]
         self.pkey = response["pkey"]
-    
+
     def export_response(self):
         if self.host and ("host" not in self.info or not self.info["host"]):
             self.info["host"] = self.host
@@ -85,9 +87,11 @@ class Client:
         if self.code:
             # set up URL parameters
             # taken from https://github.com/FreshSupaSulley/DuOSU
-            params = {"customer_protocol": "1", "pubkey": self.pubkey.publickey().export_key("PEM").decode('ascii'), "pkpush": "rsa-sha512", "jailbroken": "false", "architecture": "arm64", "region": "US", "app_id": "com.duosecurity.duomobile", "full_disk_encryption": "true", "passcode_status": "true", "platform": "Android", "app_version": "3.49.0", "app_build_number": "323001", "version": "11", "manufacturer": "unknown", "language": "en", "model": "Browser Extension", "security_patch_level": "2021-02-01"}
+            params = {"customer_protocol": "1", "pubkey": self.pubkey.publickey().export_key("PEM").decode('ascii'), "pkpush": "rsa-sha512", "jailbroken": "false", "architecture": "arm64", "region": "US", "app_id": "com.duosecurity.duomobile", "full_disk_encryption": "true",
+                      "passcode_status": "true", "platform": "Android", "app_version": "3.49.0", "app_build_number": "323001", "version": "11", "manufacturer": "unknown", "language": "en", "model": "Browser Extension", "security_patch_level": "2021-02-01"}
             # send activation request
-            r = requests.post(f"https://{self.host}/push/v2/activation/{self.code}",params=params)
+            r = requests.post(
+                f"https://{self.host}/push/v2/activation/{self.code}", params=params)
             # print(r.request.url)
 
             response = r.json()
@@ -96,22 +100,26 @@ class Client:
             raise ValueError("Code is null")
 
     def generate_signature(self, method, path, time, data):
-        message = (time + "\n" + method + "\n" + self.host.lower() + "\n" + path + '\n' + urllib.parse.urlencode(data)).encode('ascii')
+        message = (time + "\n" + method + "\n" + self.host.lower() + "\n" +
+                   path + '\n' + urllib.parse.urlencode(data)).encode('ascii')
         print(message)
 
         h = SHA512.new(message)
         signature = pkcs1_15.new(self.pubkey).sign(h)
-        auth = ("Basic "+base64.b64encode((self.pkey + ":"+base64.b64encode(signature).decode('ascii')).encode('ascii')).decode('ascii'))
+        auth = ("Basic "+base64.b64encode((self.pkey + ":" +
+                base64.b64encode(signature).decode('ascii')).encode('ascii')).decode('ascii'))
         return auth
-    
+
     def get_transactions(self):
         dt = datetime.datetime.utcnow()
         time = email.utils.format_datetime(dt)
         path = "/push/v2/device/transactions"
-        data = {"akey":self.akey,"fips_status":"1","hsm_status":"true","pkpush":"rsa-sha512"}
-        
+        data = {"akey": self.akey, "fips_status": "1",
+                "hsm_status": "true", "pkpush": "rsa-sha512"}
+
         signature = self.generate_signature("GET", path, time, data)
-        r = requests.get(f"https://{self.host}{path}",params=data, headers={"Authorization":signature, "x-duo-date":time,"host":self.host})
+        r = requests.get(f"https://{self.host}{path}", params=data, headers={
+                         "Authorization": signature, "x-duo-date": time, "host": self.host})
 
         return r.json()
 
@@ -119,26 +127,40 @@ class Client:
         dt = datetime.datetime.utcnow()
         time = email.utils.format_datetime(dt)
         path = "/push/v2/device/transactions/"+transactionid
-        data = {"akey":self.akey, "answer":answer,"fips_status":"1","hsm_status":"true","pkpush":"rsa-sha512"}
+        data = {"akey": self.akey, "answer": answer, "fips_status": "1",
+                "hsm_status": "true", "pkpush": "rsa-sha512"}
 
         # if answer == "approve":
         #     data["touch_id"] = False
         # data["push_received"] = True
         # data["pull_to_refresh_used"] = True
         signature = self.generate_signature("POST", path, time, data)
-        r = requests.post(f"https://{self.host}{path}",data=data, headers={"Authorization":signature, "x-duo-date":time,"host":self.host, "txId":transactionid})
+        r = requests.post(f"https://{self.host}{path}", data=data, headers={
+                          "Authorization": signature, "x-duo-date": time, "host": self.host, "txId": transactionid})
 
         return r.json()
+
+    def register(self, token):
+        dt = datetime.datetime.utcnow()
+        time = email.utils.format_datetime(dt)
+        path = "/push/v2/device/registration"
+        data = {"akey": self.akey, "token": token, "fips_status": "1",
+                "hsm_status": "True", "pkpush": "rsa-sha512"}
+
+        # if answer == "approve":
+        #     data["touch_id"] = False
+        # data["push_received"] = True
+        # data["pull_to_refresh_used"] = True
+        signature = self.generate_signature("POST", path, time, data)
+        r = requests.post(f"https://{self.host}{path}", data=data, headers={
+                          "Authorization": signature, "x-duo-date": time, "host": self.host})
+
 
 # c = Client(response="response.json",keyfile="mykey.pem",code="")
 # print(c)
 # #print(c.get_transactions())
 # print(c.reply_transaction("","approve"))
 
-
-
-import pathlib
-import time
 
 def main():
     code = ""
@@ -150,7 +172,7 @@ def main():
         key_exists = True
     else:
         c.export_key("key.pem")
-    
+
     if pathlib.Path("response.json").is_file() and key_exists:
         c.import_response("response.json")
         if code:
@@ -167,7 +189,7 @@ def main():
         c.read_code(code)
         c.activate()
         c.export_response()
-    
+
     while True:
         try:
             r = c.get_transactions()
@@ -175,13 +197,13 @@ def main():
             print("Connection Error")
             time.sleep(5)
             continue
-        
+
         t = r["response"]["transactions"]
         print("Checking for transactions")
         if len(t):
             for tx in t:
                 print(tx)
-                c.reply_transaction(tx["urgid"],'approve')
+                c.reply_transaction(tx["urgid"], 'approve')
                 time.sleep(2)
         else:
             print("No transactions")
